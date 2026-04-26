dc425d9af5df8aca123ff994265667bc3405791d
AdySar17 <alexanderyampier210@gmail.com>
2026-04-26 18:36:33 -0500

    Add files via upload

--- index.cjs
+++ index.cjs
@@ -1,97 +1,33 @@
 "use strict";
-var __defProp = Object.defineProperty;
-var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-var __getOwnPropNames = Object.getOwnPropertyNames;
-var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __export = (target, all) => {
-  for (var name in all)
-    __defProp(target, name, { get: all[name], enumerable: true });
-};
-var __copyProps = (to, from, except, desc) => {
-  if (from && typeof from === "object" || typeof from === "function") {
-    for (let key of __getOwnPropNames(from))
-      if (!__hasOwnProp.call(to, key) && key !== except)
-        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
-  }
-  return to;
-};
-var __reExport = (target, mod, secondTarget) => (__copyProps(target, mod, "default"), secondTarget && __copyProps(secondTarget, mod, "default"));
-var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
-
-// src/index.ts
-var index_exports = {};
-__export(index_exports, {
-  HydrationBoundary: () => import_HydrationBoundary.HydrationBoundary,
-  IsRestoringProvider: () => import_IsRestoringProvider.IsRestoringProvider,
-  QueryClientContext: () => import_QueryClientProvider.QueryClientContext,
-  QueryClientProvider: () => import_QueryClientProvider.QueryClientProvider,
-  QueryErrorResetBoundary: () => import_QueryErrorResetBoundary.QueryErrorResetBoundary,
-  infiniteQueryOptions: () => import_infiniteQueryOptions.infiniteQueryOptions,
-  mutationOptions: () => import_mutationOptions.mutationOptions,
-  queryOptions: () => import_queryOptions.queryOptions,
-  useInfiniteQuery: () => import_useInfiniteQuery.useInfiniteQuery,
-  useIsFetching: () => import_useIsFetching.useIsFetching,
-  useIsMutating: () => import_useMutationState.useIsMutating,
-  useIsRestoring: () => import_IsRestoringProvider.useIsRestoring,
-  useMutation: () => import_useMutation.useMutation,
-  useMutationState: () => import_useMutationState.useMutationState,
-  usePrefetchInfiniteQuery: () => import_usePrefetchInfiniteQuery.usePrefetchInfiniteQuery,
-  usePrefetchQuery: () => import_usePrefetchQuery.usePrefetchQuery,
-  useQueries: () => import_useQueries.useQueries,
-  useQuery: () => import_useQuery.useQuery,
-  useQueryClient: () => import_QueryClientProvider.useQueryClient,
-  useQueryErrorResetBoundary: () => import_QueryErrorResetBoundary.useQueryErrorResetBoundary,
-  useSuspenseInfiniteQuery: () => import_useSuspenseInfiniteQuery.useSuspenseInfiniteQuery,
-  useSuspenseQueries: () => import_useSuspenseQueries.useSuspenseQueries,
-  useSuspenseQuery: () => import_useSuspenseQuery.useSuspenseQuery
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
 });
-module.exports = __toCommonJS(index_exports);
-__reExport(index_exports, require("@tanstack/query-core"), module.exports);
-__reExport(index_exports, require("./types.cjs"), module.exports);
-var import_useQueries = require("./useQueries.cjs");
-var import_useQuery = require("./useQuery.cjs");
-var import_useSuspenseQuery = require("./useSuspenseQuery.cjs");
-var import_useSuspenseInfiniteQuery = require("./useSuspenseInfiniteQuery.cjs");
-var import_useSuspenseQueries = require("./useSuspenseQueries.cjs");
-var import_usePrefetchQuery = require("./usePrefetchQuery.cjs");
-var import_usePrefetchInfiniteQuery = require("./usePrefetchInfiniteQuery.cjs");
-var import_queryOptions = require("./queryOptions.cjs");
-var import_infiniteQueryOptions = require("./infiniteQueryOptions.cjs");
-var import_QueryClientProvider = require("./QueryClientProvider.cjs");
-var import_HydrationBoundary = require("./HydrationBoundary.cjs");
-var import_QueryErrorResetBoundary = require("./QueryErrorResetBoundary.cjs");
-var import_useIsFetching = require("./useIsFetching.cjs");
-var import_useMutationState = require("./useMutationState.cjs");
-var import_useMutation = require("./useMutation.cjs");
-var import_mutationOptions = require("./mutationOptions.cjs");
-var import_useInfiniteQuery = require("./useInfiniteQuery.cjs");
-var import_IsRestoringProvider = require("./IsRestoringProvider.cjs");
-// Annotate the CommonJS export names for ESM import in node:
-0 && (module.exports = {
-  HydrationBoundary,
-  IsRestoringProvider,
-  QueryClientContext,
-  QueryClientProvider,
-  QueryErrorResetBoundary,
-  infiniteQueryOptions,
-  mutationOptions,
-  queryOptions,
-  useInfiniteQuery,
-  useIsFetching,
-  useIsMutating,
-  useIsRestoring,
-  useMutation,
-  useMutationState,
-  usePrefetchInfiniteQuery,
-  usePrefetchQuery,
-  useQueries,
-  useQuery,
-  useQueryClient,
-  useQueryErrorResetBoundary,
-  useSuspenseInfiniteQuery,
-  useSuspenseQueries,
-  useSuspenseQuery,
-  ...require("@tanstack/query-core"),
-  ...require("./types.cjs")
-});
-//# sourceMappingURL=index.cjs.map
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.z = void 0;
+const z = __importStar(require("./v3/external.cjs"));
+exports.z = z;
+__exportStar(require("./v3/external.cjs"), exports);
+exports.default = z;
